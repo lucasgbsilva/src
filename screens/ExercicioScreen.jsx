ffba749e1b9a985ccc9b7d160c177e9152c80ff6
Lucas Gabriel <lucas.outro0106@gmail.com>
2023-10-17 17:22:29 -0300

    update

--- screens/ExercicioScreen.jsx
+++ screens/ExercicioScreen.jsx
@@ -3,4 +3,18 @@ import { Button, Modal } from "react-bootstrap";
 
 export default function ExercicioScreen() {
     const [show, setShow] = useState(false)
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+
+
+return(
+<>
+<Button>
+    
+</Button>
+</>
+    
+)
+
 }
